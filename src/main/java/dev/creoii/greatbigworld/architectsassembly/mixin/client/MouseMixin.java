0a3402f4793843c5f4ae44be903c53ec45010744
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-02 20:34:58 -0400

    Fix Hotbar Cycling wrong

--- src/main/java/dev/creoii/greatbigworld/architectsassembly/mixin/client/MouseMixin.java
+++ src/main/java/dev/creoii/greatbigworld/architectsassembly/mixin/client/MouseMixin.java
@@ -4,10 +4,12 @@ import com.llamalad7.mixinextras.sugar.Local;
 import dev.creoii.greatbigworld.architectsassembly.client.ArchitectsAssemblyClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.slot.SlotActionType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,13 +22,31 @@ public class MouseMixin {
     private void gbw$scrollHotbar(long window, double horizontal, double vertical, CallbackInfo ci, @Local(ordinal = 2) int k) {
         if (ArchitectsAssemblyClient.shouldCycleHotbar()) {
             if (client.player != null && client.interactionManager != null && client.player.getInventory() != null) {
+                double scrollDelta = Math.signum(k);
                 for (int i = 0; i < 9; ++i) {
-                    client.interactionManager.clickSlot(client.player.playerScreenHandler.syncId, Math.signum(k) < 0d ? i + 9 : 27, i, SlotActionType.SWAP, client.player);
-                    client.interactionManager.clickSlot(client.player.playerScreenHandler.syncId, i + 18, i, SlotActionType.SWAP, client.player);
-                    client.interactionManager.clickSlot(client.player.playerScreenHandler.syncId, Math.signum(k) < 0d ? i + 27 : 9, i, SlotActionType.SWAP, client.player);
+                    client.interactionManager.clickSlot(client.player.playerScreenHandler.syncId, cycle(i, scrollDelta > 0d ? -27 : 27), i, SlotActionType.SWAP, client.player);
+                    client.interactionManager.clickSlot(client.player.playerScreenHandler.syncId, cycle(i, scrollDelta > 0d ? -18 : 18), i, SlotActionType.SWAP, client.player);
+                    client.interactionManager.clickSlot(client.player.playerScreenHandler.syncId, cycle(i, scrollDelta > 0d ? -9 : 9), i, SlotActionType.SWAP, client.player);
                 }
             }
             ci.cancel();
         }
+    }
+
+    @Unique
+    private static int cycle(int index, int amount) {
+        int cycled = index + amount;
+
+        // -9 returns 27
+        if (cycled < 0) {
+            return PlayerInventory.MAIN_SIZE + cycled;
+        }
+
+        // 40 returns 4
+        if (cycled > PlayerInventory.MAIN_SIZE) {
+            return -PlayerInventory.MAIN_SIZE + cycled;
+        }
+
+        return cycled;
     }
 }
