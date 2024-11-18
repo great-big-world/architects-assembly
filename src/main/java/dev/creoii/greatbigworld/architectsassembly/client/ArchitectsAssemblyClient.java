18d200273d935920305b1c2a9b52287e2d0a367b
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-11-18 17:45:43 -0500

    Fix new management controls usable in spectator

--- src/main/java/dev/creoii/greatbigworld/architectsassembly/client/ArchitectsAssemblyClient.java
+++ src/main/java/dev/creoii/greatbigworld/architectsassembly/client/ArchitectsAssemblyClient.java
@@ -53,6 +53,9 @@ public class ArchitectsAssemblyClient implements ClientModInitializer {
         });
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null && client.player.isSpectator())
+                return;
+
             EXPAND_TOOLTIPS.setPressed(client.currentScreen != null && InputUtil.isKeyPressed(client.getWindow().getHandle(), EXPAND_TOOLTIPS.boundKey.getCode()));
             CYCLE_HOTBAR.setPressed(client.currentScreen == null && InputUtil.isKeyPressed(client.getWindow().getHandle(), CYCLE_HOTBAR.boundKey.getCode()));
 
