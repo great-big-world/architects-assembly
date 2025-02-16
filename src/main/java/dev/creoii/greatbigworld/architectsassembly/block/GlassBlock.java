9f3ba169ab6c32ac9f129ebed1fba26134c597ee
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-02-15 22:27:53 -0500

    Crackable colored glass
    Fix glass dropping when not silk touched oops
    Fix glass loot tables
    Fix slab translations

--- src/main/java/dev/creoii/greatbigworld/architectsassembly/block/GlassBlock.java
+++ src/main/java/dev/creoii/greatbigworld/architectsassembly/block/GlassBlock.java
@@ -4,39 +4,32 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TransparentBlock;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.component.type.ItemEnchantmentsComponent;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 public class GlassBlock extends TransparentBlock {
-    private final BlockState shattered;
+    private final BlockState cracked;
 
-    public GlassBlock(Settings settings, BlockState shattered) {
+    public GlassBlock(Settings settings, BlockState cracked) {
         super(settings);
-        this.shattered = shattered;
+        this.cracked = cracked;
     }
 
     @Override
     public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack tool) {
         super.afterBreak(world, player, pos, state, blockEntity, tool);
-        if (!player.isCreative() && EnchantmentHelper.hasEnchantments(tool)) {
-            ItemEnchantmentsComponent component = tool.getEnchantments();
-            for (RegistryEntry<Enchantment> enchantment : component.getEnchantments()) {
-                if (enchantment.hasKeyAndValue() && enchantment.getKey().get() == Enchantments.SILK_TOUCH) {
-                    world.setBlockState(pos, shattered, 2);
-                    world.emitGameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Emitter.of(state));
-                    world.syncWorldEvent(2001, pos, Block.getRawIdFromState(state));
-                    break;
-                }
-            }
+        if (EnchantmentHelper.getEnchantments(tool).getEnchantments().stream().map(entry -> entry.getKey().get()).toList().contains(Enchantments.SILK_TOUCH))
+            return;
+        if (!player.isCreative()) {
+            world.setBlockState(pos, cracked, 2);
+            world.emitGameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Emitter.of(state));
+            world.syncWorldEvent(2001, pos, Block.getRawIdFromState(state));
         }
     }
 }
