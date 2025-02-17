12e2e3a24c0d6f99b6831394cc392fdbe328d249
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-02-16 23:57:20 -0500

    Fix item variant tooltip overlaying armor bar

--- src/main/java/dev/creoii/greatbigworld/architectsassembly/mixin/client/InGameHudMixin.java
+++ src/main/java/dev/creoii/greatbigworld/architectsassembly/mixin/client/InGameHudMixin.java
@@ -1,27 +1,32 @@
 package dev.creoii.greatbigworld.architectsassembly.mixin.client;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import dev.creoii.greatbigworld.architectsassembly.variant.VariantItem;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.ColorHelper;
+import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
     @Shadow private ItemStack currentStack;
-    @Shadow public abstract TextRenderer getTextRenderer();
+    @Shadow @Final private MinecraftClient client;
 
-    @Inject(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithBackground(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIII)I"))
-    private void gbw$renderHeldItemVariants(DrawContext context, CallbackInfo ci, @Local(ordinal = 0) int i, @Local(ordinal = 1) int j, @Local(ordinal = 2) int k, @Local(ordinal = 3) int l) {
-        if (currentStack.getItem() instanceof VariantItem variantItem && !variantItem.gbw$getVariants().isEmpty()) {
-            context.drawTextWithBackground(getTextRenderer(), VariantItem.getVariantTooltip(variantItem), j, k + 10, i, ColorHelper.withAlpha(l, -1));
+    @Redirect(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithBackground(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIII)I"))
+    private int gbw$renderHeldItemVariants(DrawContext instance, TextRenderer textRenderer, Text text, int x, int y, int width, int color) {
+        if (client.player != null && client.player.getArmor() > 0) {
+            y -= 10;
         }
+        if (currentStack.getItem() instanceof VariantItem variantItem && !variantItem.gbw$getVariants().isEmpty()) {
+            instance.drawCenteredTextWithShadow(textRenderer, VariantItem.getVariantTooltip(variantItem), x + (textRenderer.getWidth(text.getString()) / 2), y + 10, color);
+        }
+        return instance.drawTextWithBackground(textRenderer, text, x, y, width, color);
     }
 }
