b7cc36c8fb06ec80b0da7d133609c12311b0276b
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-07-26 19:31:28 -0400

    Cracked glass assets & mechanics fix
    Fix a whole bunch of logspam

---
+++ src/main/java/dev/creoii/greatbigworld/architectsassembly/block/CrackedGlassBlock.java
@@ -0,0 +1,64 @@
+package dev.creoii.greatbigworld.architectsassembly.block;
+
+import dev.creoii.greatbigworld.architectsassembly.registry.ArchitectsAssemblySoundEvents;
+import dev.creoii.greatbigworld.architectsassembly.util.ArchitectsAssemblyTags;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TransparentBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+
+import java.util.function.Supplier;
+
+public class CrackedGlassBlock extends TransparentBlock {
+    private final Supplier<BlockState> fixed;
+    private final Supplier<Item> shard;
+
+    public CrackedGlassBlock(Settings settings, Supplier<BlockState> fixed, Supplier<Item> shard) {
+        super(settings);
+        this.fixed = fixed;
+        this.shard = shard;
+    }
+
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        ItemStack stack = player.getStackInHand(player.getActiveHand());
+        if (stack.isOf(shard.get())) {
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, state));
+            world.setBlockState(pos, fixed.get(), 3);
+            world.playSound(player, pos, ArchitectsAssemblySoundEvents.BLOCK_GLASS_REPAIR, SoundCategory.BLOCKS, 1f, 1f);
+            stack.decrementUnlessCreative(1, player);
+            if (!world.isClient)
+                return ActionResult.SUCCESS_SERVER;
+            return ActionResult.SUCCESS;
+        }
+        return super.onUse(state, world, pos, player, hit);
+    }
+
+    @Override
+    public void onProjectileHit(World world, BlockState state, BlockHitResult hit, ProjectileEntity projectile) {
+        if (!world.isClient) {
+            BlockPos pos = hit.getBlockPos();
+            if (projectile.canModifyAt((ServerWorld) world, pos) && projectile.canBreakBlocks((ServerWorld) world) && projectile.getVelocity().length() > .4d) {
+                world.breakBlock(pos, true, projectile);
+            }
+        }
+    }
+
+    @Override
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, double fallDistance) {
+        entity.handleFallDamage(fallDistance * .8f, .5f, entity.getDamageSources().fall());
+        if (!world.isClient && fallDistance >= .6f && !entity.getType().isIn(ArchitectsAssemblyTags.LIGHTWEIGHT_ENTITIES)) {
+            world.breakBlock(pos, !(entity instanceof PlayerEntity player) || !player.isCreative(), entity);
+        }
+    }
+}
