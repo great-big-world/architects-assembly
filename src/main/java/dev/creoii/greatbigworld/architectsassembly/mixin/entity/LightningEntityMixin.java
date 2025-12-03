4476c0e64197ba988a95a9a6c66b9789d3a358dd
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-12-02 19:44:58 -0500

    Add screenshake to explosions & lightning

---
+++ src/main/java/dev/creoii/greatbigworld/architectsassembly/mixin/entity/LightningEntityMixin.java
@@ -0,0 +1,30 @@
+package dev.creoii.greatbigworld.architectsassembly.mixin.entity;
+
+import dev.creoii.greatbigworld.client.ScreenShake;
+import dev.creoii.greatbigworld.util.network.ScreenShakeS2C;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LightningEntity.class)
+public abstract class LightningEntityMixin extends Entity {
+    public LightningEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LightningEntity;emitGameEvent(Lnet/minecraft/registry/entry/RegistryEntry;)V"))
+    private void gbw$lightningScreenShake(CallbackInfo ci) {
+        PlayerLookup.tracking((ServerWorld) getEntityWorld(), getBlockPos()).forEach(serverPlayer -> {
+            float t = Math.max(0f, 2.5f - ((float) serverPlayer.getEntityPos().distanceTo(getEntityPos()) / 64f));
+            ServerPlayNetworking.send(serverPlayer, new ScreenShakeS2C(t, 200, ScreenShake.Easing.OUT));
+        });
+    }
+}
---
+++ src/main/java/dev/creoii/greatbigworld/architectsassembly/mixin/world/ExplosionImplMixin.java
@@ -0,0 +1,28 @@
+package dev.creoii.greatbigworld.architectsassembly.mixin.world;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import dev.creoii.greatbigworld.client.ScreenShake;
+import dev.creoii.greatbigworld.util.network.ScreenShakeS2C;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.explosion.ExplosionImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ExplosionImpl.class)
+public abstract class ExplosionImplMixin {
+    @Shadow
+    public abstract Vec3d getPosition();
+
+    @Inject(method = "damageEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;addVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
+    private void gbw$explosionScreenShake(CallbackInfo ci, @Local Entity entity) {
+        if (entity instanceof ServerPlayerEntity serverPlayer) {
+            ServerPlayNetworking.send(serverPlayer, new ScreenShakeS2C((float) Math.exp(-.08f * serverPlayer.getEntityPos().distanceTo(getPosition())), 80, ScreenShake.Easing.OUT));
+        }
+    }
+}
